882a09507b2fce28431230515cbc83079892b0b6
Wesley Leon <theduckster1231@gmail.com>
2026-09-08 14:56:33 -0400

    Refactor Solution class to count commas in number

--- today/Solution.cs
+++ today/Solution.cs
@@ -6,12 +6,17 @@
 //
 // Approach:
 //
-// Time:  O()
-// Space: O()
+// Time:  O(logn)
+// Space: O(logn)
 
-public class Solution
-{
-    public void MethodName()
-    {
+public class Solution {
+    public int CountCommas(int n) {
+        return CountCommaHelper(n);
+    }
+    public int CountCommaHelper(int n){
+        if (n < 1000)return 0; 
+        int commas = (int)(Math.Log10(n)/3);
+        int count = n - (int)Math.Pow(1000, commas) + 1;
+        return commas * count + CountCommaHelper((int)Math.Pow(1000, commas)-1);
     }
 }
