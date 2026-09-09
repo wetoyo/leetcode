815c9a1ac715a7abffa7ab3e89f27b41756332cb
Wesley Leon <theduckster1231@gmail.com>
2026-09-09 10:41:20 -0400

    Update Solution.cs

--- today/Solution.cs
+++ today/Solution.cs
@@ -1,17 +1,30 @@
 // LeetCode #3871 - Count Commas in Range II
-// https://leetcode.com/problems/count-commas-in-range-ii/
+// https://leetcode.com/problems/count-commas-in-range/
 //
 // Difficulty: Medium
 // Topics: Math
 //
 // Approach:
 //
-// Time:  O()
-// Space: O()
+// Time:  O(logn)
+// Space: O(logn)
 
-public class Solution
-{
-    public void MethodName()
-    {
+public class Solution {
+    public long CountCommas(long n) {
+        return CountCommaHelper(n);
+    }
+    public long CountCommaHelper(long n){
+        if (n < 1000)return 0; 
+        long commas = 0;
+        long temp = n;
+        long pwr = 1;
+        while (temp >= 1000)
+        {
+            temp/=1000;
+            pwr*=1000;
+            commas++;
+        }
+        long count = n - pwr +1;
+        return commas * count + CountCommaHelper(pwr-1);
     }
 }
