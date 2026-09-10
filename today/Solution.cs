24b4adb304b35ef6c93a147ea743e7cb42904769
Wesley Leon <theduckster1231@gmail.com>
2026-09-10 14:59:09 -0400

    Update Solution.cs

--- today/Solution.cs
+++ today/Solution.cs
@@ -6,12 +6,29 @@
 //
 // Approach:
 //
-// Time:  O()
-// Space: O()
+// Time:  O(n?)
+// Space: O(n?)
+// not sure about the complexity, but each node is explored only once, so I believe that it is o(n)
+public class Solution {
+    public int count = 0;
 
-public class Solution
-{
-    public void MethodName()
-    {
+    public int AverageOfSubtree(TreeNode root) {
+        Recurse(root);
+        return count;
+    }
+
+    public (int, int) Recurse(TreeNode root) {
+        if (root == null) return (0, 0);
+
+        (int, int) left = Recurse(root.left);
+        (int, int) right = Recurse(root.right);
+
+        int sum = left.Item1 + right.Item1 + root.val;
+        int nodes = left.Item2 + right.Item2 + 1;
+
+        if (sum / nodes == root.val)
+            count++;
+
+        return (sum, nodes);
     }
 }
