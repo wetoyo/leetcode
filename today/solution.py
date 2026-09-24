bb191521059b5f6d0157a2c76fef330d2cb39100
Wesley Leon <theduckster1231@gmail.com>
2026-09-24 14:32:11 -0400

    Update solution.py

--- today/solution.py
+++ today/solution.py
@@ -8,15 +8,27 @@ Topics: Array, Math
 Approach:
 
 
-Time:  O()
-Space: O()
+Time:  O(n)
+Space: O(1)
 """
 
 
-class Solution:
-    def method_name(self):
-        pass
-
+class Solution(object):
+    def smallestIndex(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        for i in range(len(nums)):
+            total = 0
+            temp = nums[i]
+            while (temp > 0):
+                total += temp % 10
+                temp /= 10
+                if (total > i): break
+            if (total == i): return i
+        return -1
+            
 
 if __name__ == "__main__":
     sol = Solution()
